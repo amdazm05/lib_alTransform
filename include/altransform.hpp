089af38acf05e42d18e026d2cfea1b35ac48bea6
Ahmed Azeem <ahmed.azeem@rwrlimited.com>
2022-09-23 18:15:47 +0500

    added header for transform class

---
+++ include/altransform.hpp
@@ -0,0 +1,12 @@
+#ifndef __ALTRANFORM
+#define __ALTRANFORM
+#include <boost/numeric/ublas/matrix.hpp>
+
+class AlTransform
+{
+    private:
+
+    public:
+};
+
+#endif
--- src/main.cc
+++ src/main.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <altransform.hpp>
 int main()
 {
     return 0;
